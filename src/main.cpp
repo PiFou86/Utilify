dbf53df844390f5bc22e2713dbf481ec867178a4
Pierre-François Léon <pierrefrancois.leon@gmail.com>
2024-12-22 17:47:20 -0500

    Pb noms

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 
-#include "Utilify/StringUtil.h"
+#include "Utilify/StringUtils.h"
 
 void setup() { 
     Serial.begin(9600);
-    Serial.println(StringUtil::padLeft("Hello", ' ', 10));
-    Serial.println(StringUtil::padLeft(String(42), '0', 4));
-    Serial.println(StringUtil::padLeft("0x" + String(42), '0', 4));
+    Serial.println(StringUtils::padLeft("Hello", ' ', 10));
+    Serial.println(StringUtils::padLeft(String(42), '0', 4));
+    Serial.println(StringUtils::padLeft("0x" + String(42), '0', 4));
 }
 
 void loop() { ; }
