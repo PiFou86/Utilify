83a9bf26b65b1c6b0d6efa72562437945e8d6e7f
Pierre-François Léon <pierrefrancois.leon@gmail.com>
2025-04-26 19:53:21 -0400

    Ajout couleurs pour terminal

---
+++ include/Utilify/TerminalConstants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// ANSI colors for terminal visualization
+#define USE_COLOR_TERMINAL
+#define TEXT_BLUE "\033[34m"
+#define TEXT_GREEN "\033[32m"
+#define TEXT_RED "\033[31m"
+#define TEXT_YELLOW "\033[33m"
+#define TEXT_RESET "\033[0m"
+#define TEXT_BG_BLACK "\033[40m"
+#define TEXT_BOLD "\033[1m"
